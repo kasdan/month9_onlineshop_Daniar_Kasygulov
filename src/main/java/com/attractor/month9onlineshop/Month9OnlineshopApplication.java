82bb6d53905e06619d81495608d57576748c6ba5
kasdan <kasdan23@gmail.com>
2022-05-04 21:11:51 +0600

    Initial commit

---
+++ src/main/java/com/attractor/month9onlineshop/Month9OnlineshopApplication.java
@@ -0,0 +1,13 @@
+package com.attractor.month9onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Month9OnlineshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Month9OnlineshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attractor/month9onlineshop/Month9OnlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attractor.month9onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Month9OnlineshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
