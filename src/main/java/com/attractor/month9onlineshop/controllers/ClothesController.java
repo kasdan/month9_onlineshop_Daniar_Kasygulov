a48a2737d79f1aaeaf6f3e088d85b595dde3ffb1
kasdan <kasdan23@gmail.com>
2022-05-10 17:18:25 +0600

    HW 66, task 3 added show clothes into controller and services

--- src/main/java/com/attractor/month9onlineshop/controllers/ClothesController.java
+++ src/main/java/com/attractor/month9onlineshop/controllers/ClothesController.java
@@ -2,10 +2,19 @@ package com.attractor.month9onlineshop.controllers;
 
 import com.attractor.month9onlineshop.services.ClothesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+
+@Controller
 @RequiredArgsConstructor
 public class ClothesController {
     private final ClothesService clothesService;
+
+    @GetMapping("/clothes")
+    public String getAllClothes(Model model){
+        model.addAttribute("clothes",clothesService.getListOfClothes());
+        return "clothes";
+    }
 }
