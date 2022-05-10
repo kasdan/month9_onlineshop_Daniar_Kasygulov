56ecae0a9bd95750b1b8a30bfef5d40eb63d2dec
kasdan <kasdan23@gmail.com>
2022-05-10 17:59:43 +0600

    HW 66, task 3 updated show clothes

--- src/main/java/com/attractor/month9onlineshop/Month9OnlineshopApplication.java
+++ src/main/java/com/attractor/month9onlineshop/Month9OnlineshopApplication.java
@@ -2,6 +2,11 @@ package com.attractor.month9onlineshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Month9OnlineshopApplication {
