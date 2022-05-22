cdfbcf872ad2cebab6b2cb3a41595df936b9c0db
kasdan <kasdan23@gmail.com>
2022-05-22 23:49:23 +0600

    Lab 70, Task 2 Added flyway migration version for comments

---
+++ src/main/resources/db/migration/V015__Initial_setup_comments.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+                            `id` bigint NOT NULL AUTO_INCREMENT,
+                            `comment_text` varchar(255) NOT NULL,
+                            `order_id` bigint NOT NULL,
+                            PRIMARY KEY (`id`),
+                            KEY `orders_idx` (`order_id`),
+                            CONSTRAINT `orders` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
